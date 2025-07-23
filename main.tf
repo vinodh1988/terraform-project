f62e531b5e5f2f5845d4b8f3f5574b3a4c74e8a0
vinodh1988 <k.c.vinodh@hotmail.com>
2025-07-23 15:27:09 +0530

    latest

--- main.tf
+++ main.tf
@@ -7,3 +7,13 @@ resource "aws_s3_bucket" "s3bucket" {
         Environment = "Dev"
     }
 }
+
+resource "aws_s3_bucket" "s3bucket" {
+    bucket = "awssuperterra348034bucket"
+    acl    = "private"
+
+    tags = {
+        Name        = "bucketterra"
+        Environment = "Dev"
+    }
+}

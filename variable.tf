7fb4da98ea39132de72d2d85c8aa33fe53235fb8
vinodh1988 <k.c.vinodh@hotmail.com>
2025-07-23 17:14:42 +0530

    latest

--- variable.tf
+++ variable.tf
@@ -1,3 +1,28 @@
 variable "region" {
     default = "ap-south-1"
 }
+
+variable "rules" {
+    type=list(object({
+        from_port = number 
+        to_port = number
+        protocol = string
+        cidr_block = string
+        description = string
+    }))
+
+    default = [{
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_block = "0.0.0.0/0"
+        description = "allow ssh connectivity"
+    },
+     {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_block = "0.0.0.0/0"
+        description = "allow web server connectivity"
+     }]
+}
